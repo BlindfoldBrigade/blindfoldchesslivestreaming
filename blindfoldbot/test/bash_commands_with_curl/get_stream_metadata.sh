ec717bb5964034fe5e7b10479f60dd3de43c9324
Jamie Campbell <jamie@parit.ca>
2018-06-14 20:00:14 -0700

    taking out old location for bash curl approach

--- blindfoldbot/test/bash_commands_with_curl/get_stream_metadata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#1) recommended to define the variables in the environment for instance with sourcing a script, eg, . variable_exports.sh
-curl -H "Client-ID $TWITCH_CLIENT_ID" -H "Authorization: Bearer $TWITCH_BEARER" -X GET 'https://api.twitch.tv/helix/streams/metadata?community_id=4912794f-4830-470f-8224-a2f793d4e5b6'
-
--- blindfoldbot/test/bash_commands_with_curl/not_working_yet/create_entitlement_grants_upload_url.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#Need an "app token" to do this, not the regular access token, so the command as written below doesn't work.
-#1) recommended to define the variables in the environment for instance with sourcing a script, eg, . variable_exports.sh
-#curl -H "Client-ID $TWITCH_CLIENT_ID" -H "Authorization: Bearer $TWITCH_BEARER" -X POST 'https://api.twitch.tv/helix/entitlements/upload?manifest_id=7658&type=bulk_drops_grant'
-
-
